12045102ac63dc4a8ccdb0a0b9bc897d1853bfc6
bigwanluther <bigwanluther@gmail.com>
2025-03-26 22:53:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,180 @@
+// let footballer={
+//     firstName:"Bigwan",
+//     lastName:"Luther",
+//     age:25,
+//     club:"chelsea",
+//     couch:"Bigwan ishaya"
+// }
+// // console.log(footballer.firstName)
+// let footballers=
+// [
+//     {
+//         name:'Ronaldo',
+//         age:25,
+//         location:'argentina'
+
+//     },
+//     {
+//         name:'messi',
+//         age:30,
+//         location:'india'
+//     },
+//     {
+//         name:'luther',
+//         age:25,
+//         location:null
+//     }
+// ]
+// console.log(typeof [])
+
+// let number=4
+// myBoolean=false
+// myString='variables are great'
+// console.log(number,myBoolean,myString)
+    // var myArray=['hello',5,45 ,100 ,15 ]
+    // console.log(length)
+
+// let random='what is the meaning of life ?'
+// number=42
+// Boolean=true
+
+// console.log(`Answer this question: ${random}`)
+
+// let number=[]
+// console.log(number)
+
+// let todos=[
+//     "blockfuse",
+//     "wash",
+//     'sleep',
+//     'skate'
+// ]
+// console.log(todos[3])
+
+// let array1=[1,2,4,6,9,3,6];
+// let firstItem =array1[0]
+// let seconItem=array1[1]
+// let thirdItem=array1[2]
+// let fourthItem=array1[3]
+// let fifthItem=array1[4]
+// let sixItem=array1[5]
+// let seventhItem=array1[6]
+//  console.log(firstItem,seconItem,thirdItem,fourthItem,fifthItem,sixItem,seventhItem)
+
+
+
+
+//  let angwa=['angwa','rayfield','bukuru','rusau','hwolshe','lowcost','dong'];
+//  let tina=angwa[0]
+//  let rayfield=angwa[1]
+//  let bukuru=angwa[2]
+//  let rusau=angwa[3]
+//  let hwolshe=angwa[4]
+//  let lowcost=angwa[5]
+//  let dong=angwa[6]
+// //  console.log(tina,rayfield,bukuru,rusau,hwolshe,lowcost,dong)
+
+// // console.log(angwa.length)
+// console.log(angwa.length-1)
+
+
+// let age=2
+// let rand=5
+// age+=rand
+// let result=age+rand
+// console.log(result)
+
+// let age=2
+// let rand=5
+// console.log('initial values: Age= ${age},rand =${rand}')
+// age+rand
+// console.log('first operation: Age= ${age},rand =${rand}')
+// let result=age+rand
+// console.log('final operation: Age= ${age},rand =${rand}')
+// console.log("result:",result)
+
+
+// let score=20
+// let deduct=21
+// score-=deduct
+// console.log(score)
+
+
+// let a=10
+// let b=5
+// let c=2
+// a*=(c*b)
+
+// console.log(a)
+
+
+// let a=0
+// let b=10
+// let c=5
+// a/=(c/b)
+// console.log(a)
+
+
+// let a=10
+// let b=3
+// let c=2
+
+// console.log(a%b)
+
+
+// let c=2
+// let a=10
+// let b=3
+// a**=b
+// console.log(a)
+
+
+
+// let a=100
+// let b=50
+// let c=3
+// a+=b
+// console.log(a)
+
+
+// let a=100
+// let b=50
+// let c=3
+// a-=b
+// console.log(a)
+
+
+// let a=10
+// let b=3
+// let c=2
+// a*=b
+// console.log(a)
+
+
+// let a=10
+// let b=3
+// let c=2
+// a/=b
+// console.log(a)
+
+
+
+// let a=10
+// let b=3
+// let c=2
+// a-=c
+// console.log(a)
+
+
+// let a=10
+// let b=3
+// let c=2
+// a*=c
+// console.log(a)
+
+
+let a=5
+let b=20
+let c=10
+let d=5
+console.log(a!=c)
