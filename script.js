3a53342532ba78cdaed38c88209029a36de6d6aa
bigwanluther <bigwanluther@gmail.com>
2025-03-31 22:46:06 +0100

    first commit

--- script.js
+++ script.js
@@ -64,6 +64,7 @@
 
 
 
+
 //  let angwa=['angwa','rayfield','bukuru','rusau','hwolshe','lowcost','dong'];
 //  let tina=angwa[0]
 //  let rayfield=angwa[1]
@@ -173,8 +174,160 @@
 // console.log(a)
 
 
-let a=5
-let b=20
-let c=10
-let d=5
-console.log(a!=c)
+// let a=10
+// let b=5
+// let c=11
+// console.log(a<=c)
+
+
+// let a=10
+// let b=5
+// let c=10
+// let d=15
+// let e=11
+// console.log(a>c)
+
+//     //  todays task.(assignment).
+// let a = 5, b = 10, c = 20, d = 13, e = 5, f = 13, g = 14;
+
+// // 1. Is "a" equal to "b"?
+// console.log("1.", a === b);
+
+// // 2. Is "a * b" equal to "d"?
+// console.log("2.", (a * b) === d);
+
+// // 3. Is "a % b" equal to "c % b"?
+// console.log("3.", (a % b) === (c % b))
+
+// // 4. Is "d" greater than "f"?
+// console.log("4.", d > f);
+
+// // 5. Is "a + b + c" equal to "g * 5"?
+// console.log("5.", (a + b + c) === (g * 5));
+
+// // 6. What will be the output of "(2 + 3) * (4 - 1)"?
+// console.log("6.", (2 + 3) * (4 - 1)); // 15
+
+// // 7. What will be the output of 5 == "5"?
+// console.log("7.", 5 == "5");
+
+// // 8. Program that declares two numbers and prints sum, difference, product, quotient, remainder
+// let num1 = 10, num2 = 3;
+// console.log("8. Sum:", num1 + num2);
+// console.log("   Difference:", num1 - num2);
+// console.log("   Product:", num1 * num2);
+// console.log("   Quotient:", num1 / num2);
+// console.log("   Remainder:", num1 % num2);
+
+// // 9.
+// //  Program to take a number and print its square
+// let number = 4;
+// console.log("9. Square of", number, ":", number ** 2);
+
+// // 10. Program to take a number and print its square root
+// let num = 25;
+// console.log("10. Square Root of", num, ":", Math.sqrt(num));
+
+// let name="luther"
+// let greetings="good morning "+name
+// console.log(greetings)
+
+// let ban="Apples"
+// let app="apples"
+// console.log(app>ban)
+
+
+// console.log(2+4+"5")
+
+// let score=70
+// let grade
+
+// if(score>=70){
+//     grade="A"
+    
+// }else if (score>=60 && score<=100){
+//     grade="B"
+// }else if (score>=50 && score<=59){
+//     grade="C"
+// }else if (score>=40 && score<=49){
+//     grade="D"
+// }else if (score>=0){
+//     grade="fail"
+// }
+// console.log(grade)
+
+// loops( learning how to print a multiple word or number using loops)
+// for(let i=1; i <= 100; i++ ){
+//     console.log("blockfuse labs")
+// }
+
+
+// using an integer say a whole number
+// we will print all the even and odd numbers
+
+// let number=100;
+
+// for (let i=i; i <= 10; i++) {
+//     if (i%2==0) {
+//         console.log(`the value ${i}is an even number`);
+//     }else{
+//         console.log(`the number ${i} is an odd number`);
+//     }
+// }
+
+// [2,4,6,8,10,12] look through the array while looking be multiplying each number by 3
+
+let numbers = [2, 4, 6, 8, 10, 12];
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i] *= 3;
+}
+console.log(numbers);
+
+
+// factoria of 10!=10*9*8*7*6*5*4*3*2*1 using loop.
+// using for
+let num = 10;
+let factorial = 1;
+
+for (let i = num; i >= 1; i--) {
+    factorial *= i;
+}
+
+console.log(` using for loop=Factorial of ${num} is: ${factorial}`);
+
+
+// using while
+let numb = 10;
+let factoriall = 1;
+let i = num;
+
+while (i > 0) {
+    factoriall *= i;
+    i--;
+}
+
+console.log(` using while loop =Factorial of ${numb} is: ${factoriall}`); // Output: 3628800
+
+// print two different arrays of 100 finding the odd number for the other array and even numbers for the other array.
+
+let oddNumbers = [];
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 !== 0) {
+        oddNumbers.push(i);
+    }
+}
+
+console.log("Odd Numbers:", oddNumbers);
+
+
+let evenNumbers = [];
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        evenNumbers.push(i);
+    }
+}
+
+console.log("Even Numbers:", evenNumbers);
